f481f8dd637bbcf666804427bd07d08ac17fc47e
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-07 18:17:53 +0530

    import Navbar in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
-import Home from './pages/Home'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
+      <Navbar />
       <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
