cdd76f3790a195f7f0228e370bd24bcf8d1a4422
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-07 19:50:44 +0530

    feat: make projects routes and add route for 404 error

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,11 +1,14 @@
 import { Route , Routes} from "react-router-dom";
 import Home from "./pages/Home";
+import Project from "./pages/Project";
 
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Project />} />
+      <Route path="*" element={<div className="text-white text-center py-20">404 - Page Not Found</div>} />
     </Routes>
   );
 }
