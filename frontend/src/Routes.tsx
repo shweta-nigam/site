0246e1da4d648f4fa2a0283f50674c7a557189ea
Shweta Nigam <shwetanigam2106@gmail.com>
2025-11-18 18:36:33 +0530

    feat: add contact page to Routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,6 +2,7 @@ import { Route , Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 
 function AppRoutes() {
@@ -10,6 +11,7 @@ function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/projects" element={<Project />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<div className="text-white text-center py-20">404 - Page Not Found</div>} />
     </Routes>
   );
